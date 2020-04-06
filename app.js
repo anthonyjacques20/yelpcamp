b18a96e42d74e18546721f379efb8290ff4f534d
Anthony Jacques <ajaxjjj20@gmail.com>
2020-04-06 22:19:04 +0000

    Change database connect URL to use environment variables

--- app.js
+++ app.js
@@ -20,8 +20,7 @@ var commentRoutes = require("./routes/comments");
 var indexRoutes = require("./routes/index");
 
 //Connect to database
-//mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
-mongoose.connect("mongodb+srv://mongoDBUser:" + process.env.DB_PASS + "@anthonyscluster-secvg.mongodb.net/test?retryWrites=true&w=majority", 
+mongoose.connect(process.env.DATABASEURL, 
 	{
 		useNewUrlParser: true,
 		useCreateIndex: true,
