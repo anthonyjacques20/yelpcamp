12a4725333cb96a368c0ae69263135a93f06539b
Anthony Jacques <ajaxjjj20@gmail.com>
2020-04-21 05:21:00 +0000

    Add cloudinary support so that we can upload images.  Closes #8

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -7,6 +7,32 @@ var Campground = require("../models/campground");
 //So these two lines are the same
 //var middleware = require("../middleware/index");
 var middleware = require("../middleware");
+//=======================
+//Image upload code
+var multer = require("multer");
+var storage = multer.diskStorage({
+	//Create a custom name for the uploaded file
+	filename: function(req, file, callback) {
+		callback(null, Date.now() + file.originalname);
+	}
+});
+//Only allow certain file types
+var imageFilter = function (req, file, cb) {
+	// accept image file sonly
+	if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+		return cb(new Error('Only image files are allowed!'), false);
+	}
+	cb(null, true)
+};
+var upload = multer({storage: storage, fileFilter: imageFilter})
+
+var cloudinary = require('cloudinary');
+cloudinary.config({
+	cloud_name: 'anthonyjacques20',
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET
+});
+//=======================
 
 //=======================
 //Campground Routes
@@ -25,29 +51,24 @@ router.get("/", function(req, res){
 });
 
 //CREATE - Add a new one to the DB
-router.post("/", middleware.isLoggedIn, function(req, res){
-	//Grab the name and image from the body
-	var name = req.body.name;
-	var image = req.body.image;
-	var description = req.body.description;
-	var price = req.body.price;
-	//Pass author to new Campground
-	var author = {
-		id: req.user._id,
-		username: req.user.username
-	};
-	//Create a new object
-	var newCampground = {name: name, image: image, description: description, author: author, price: price};
-	//Save campground to DB
-	Campground.create(newCampground, function(err, newlyCreatedCampground){
-		if(err){
-			console.log(err);
+router.post("/", middleware.isLoggedIn, upload.single('image'), function(req, res){
+	//Put this code in the callback for geocoder.geocode() callback for google maps
+	cloudinary.uploader.upload(req.file.path, function(result){
+		//Add cloudinary url for the image to the campground object under image property
+		req.body.campground.image = result.secure_url;
+		//Add author to campground
+		req.body.campground.author = {
+			id: req.user._id,
+			username: req.user.username
 		}
-		else{
-			console.log(newlyCreatedCampground);
-			res.redirect("/campgrounds");
-		}
-	})
+		Campground.create(req.body.campground, function(err, campground){
+			if(err){
+				req.flash('error', err.message);
+				return res.redirect('back');
+			}
+			res.redirect('/campgrounds/' + campground.id);
+		});
+	});
 });
 
 //NEW - show new form
